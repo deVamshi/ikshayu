87871a4f8cb20ce38b375e271cbed1eea258bdb5
Yaswi <gchali60@gmail.com>
2022-08-27 16:00:40 +0530

    configured firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,14 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:sensors_plus/sensors_plus.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
